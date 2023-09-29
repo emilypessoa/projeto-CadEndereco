605efd195549753037460ed7cb416182204e12b7
Emily Pessoa <emymi2007@gmail.com>
2023-09-29 10:16:37 -0300

    construindo a função de API

--- script.js
+++ script.js
@@ -19,3 +19,12 @@ const preencherFormulario = (endereco) =>{
     document.getElementById('cidade').value = endereco.localidade;
     document.getElementById('estado').value = endereco.uf;
 }
+
+/*
+Função para consumo de API
+utilizando a função do tipo assincrona
+*/
+const pesquisarcep = async() =>{
+    limparFormulario();
+    const url = `http://viacep.com.br/ws/${cep.value}/json/`;
+}
